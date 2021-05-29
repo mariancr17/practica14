f2f9638f60148cb093d00a157bb236d409f54b15
mariancr17 <84986815+mariancr17@users.noreply.github.com>
2021-05-29 16:16:14 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
-//EJERCICIO UNO
+//EJERCICIO DE CÁLCULO
 function limpiarFormulario(){
 	 document.getElementById("miForm").reset();
 }
-	 var comparar = function(n1, n2, n3){
-	
- var numero1 = parseInt(document.getElementById("numero1").value);
- var numero2 = parseInt(document.getElementById("numero2").value);
- var numero3 = parseInt(document.getElementById("numero3").value);
- 
- if(numero1 <= numero2 && numero1 <= numero3)
- 	menor = numero1;
- else if(numero2 < numero1 && numero2 <= numero3)
- 	menor = numero2;
- else
- 	menor = numero3;
+	 var calcular = function(e1){
 
- return menor;
+	 var funcion1 = parseInt(document.getElementById("ecuacion").value);
+
+	 var resultado = funcion1 * 0;
+
+	 alert ("La derivada de una constante siempre es " +resultado);
 }
 
-//EJERCICIO DOS
+
+//EJERCICIO DE FÍSICA
 function limpiarFormulario(){
 	 document.getElementById("miForm").reset();
 }
-	 var conversion = function(d1){
+	 var div = function(d, t){
 	
- var distancia1 = parseInt(document.getElementById("distancia1").value);
- var resultado = distancia1 / 2.54;
+ var desplazamiento1 = parseInt(document.getElementById("desplazamiento").value);
+ var tiempo1 = parseInt(document.getElementById("tiempo").value);
+
+ var resultado = desplazamiento1 / tiempo1;
 
  return resultado;
 }
-
-//EJERCICIO TRES
-function limpiarFormulario(){
-	 document.getElementById("miForm").reset();
-}
-     var cambio = function(l1){
-
-var longitud1 = parseInt(document.getElementById("longitud1").value);
-var resultado = longitud1 * 1.0936;
-
-return resultado;
-} 
